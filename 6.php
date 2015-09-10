ec51ebe361801303efb67e172bd1f82d93ebfe14
jhananim <jhanani@ideas2it.com>
2015-09-10 15:19:47 +0530

    4

---
+++ 6.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+function names($name,$year=1999)
+{
+echo "Mr. $name was born in the year $year<br>";
+}
+names("aalok","1975");
+names("ishaan","1980");
+names("suraj","1990");
+names("samrat");
+?>
+</body>
+</html>
